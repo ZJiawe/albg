13ca61808650672ccc20452a7b59dbd040105f3f
zhengjw <zhengjw@startdima.com.cn>
2020-07-20 21:40:38 +0800

    添加网关服务相关代码

---
+++ basic-service/cloud-gateway9527/src/main/java/com/albg/springcloud/config/GateWayConfig.java
@@ -0,0 +1,12 @@
+package com.albg.springcloud.config;
+
+/**
+ * @Auther: MrZheng
+ * @Date: 2020/7/20 21:24
+ * @Description:
+ */
+public class GateWayConfig {
+
+
+
+}
---
+++ basic-service/cloud-gateway9527/src/main/java/com/albg/springcloud/filter/MyLogGateWayFilter.java
@@ -0,0 +1,12 @@
+package com.albg.springcloud.filter;
+
+/**
+ * @Auther: MrZheng
+ * @Date: 2020/7/20 21:24
+ * @Description:
+ */
+public class MyLogGateWayFilter {
+
+
+
+}
