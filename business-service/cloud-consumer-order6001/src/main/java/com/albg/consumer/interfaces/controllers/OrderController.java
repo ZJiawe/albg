fd5fdd41a1ab9d6122bad3049cfc87ccd1aadd8e
zhengjw <zhengjw@startdima.com.cn>
2020-07-25 16:53:27 +0800

    添加zipkin以及相关笔记

--- business-service/cloud-consumer-order6001/src/main/java/com/albg/consumer/interfaces/controllers/OrderController.java
+++ business-service/cloud-consumer-order6001/src/main/java/com/albg/consumer/interfaces/controllers/OrderController.java
@@ -35,4 +35,11 @@ public class OrderController {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
     }
 
+    @GetMapping("/consumer/payment/zipkin")
+    public String paymentZipkin()
+    {
+        String result = restTemplate.getForObject(PAYMENT_URL+"/payment/zipkin/", String.class);
+        return result;
+    }
+
 }
