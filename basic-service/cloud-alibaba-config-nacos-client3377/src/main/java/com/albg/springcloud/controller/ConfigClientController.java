a7a37fb88230dfdf2046f7f2fa699dc337e939ed
zhengjw <zhengjw@startdima.com.cn>
2020-07-28 21:28:51 +0800

    添加nacos动态刷新功能

---
+++ basic-service/cloud-alibaba-config-nacos-client3377/src/main/java/com/albg/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.albg.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: MrZheng
+ * @Date: 2020/7/28 20:57
+ * @Description:
+ */
+@RestController
+@RefreshScope //支持Nacos的动态刷新功能。
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+
+}
