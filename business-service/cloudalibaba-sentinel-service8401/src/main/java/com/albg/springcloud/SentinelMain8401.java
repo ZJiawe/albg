752b7047b4a57b72766f308b47fad0715190a211
zhengjw <zhengjw@startdima.com.cn>
2020-08-02 18:13:37 +0800

    添加sentinel相关服务代码

---
+++ business-service/cloudalibaba-sentinel-service8401/src/main/java/com/albg/springcloud/SentinelMain8401.java
@@ -0,0 +1,20 @@
+package com.albg.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Auther: MrZheng
+ * @Date: 2020/8/2 15:53
+ * @Description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SentinelMain8401 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelMain8401.class, args);
+    }
+
+}
